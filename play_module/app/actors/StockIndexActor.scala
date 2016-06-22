624b6e622439d5e33368bc7712e81ec64060be9d
Piotr Kluch <206799@edu.p.lodz.pl>
2016-06-22 20:28:50 +0200

    Prepare StockIndexActor for newer implementation

--- play_module/app/actors/StockIndexActor.scala
+++ play_module/app/actors/StockIndexActor.scala
@@ -6,9 +6,9 @@ import akka.routing.RoundRobinPool
 sealed trait StockIndexMessage
 
 case object Calculate extends StockIndexMessage
-case class Work(number: Double) extends StockIndexMessage
+case class Work(indexName: IndexName, companyName: String) extends StockIndexMessage
 case class PartialResult(value: Double) extends StockIndexMessage
-case class StockIndexValue(value: Double, tickerSymbol: String)
+case class StockIndexValue(listOfResults: ListBuffer[Double], indexName: IndexName, companyName: Seq[String])
 
 /**
   * The main object for stock index calculations
@@ -33,54 +33,115 @@ object StockIndexActor extends App {
     val listener = system.actorOf(Props[Listener], name = "listener")
 
     // Create the master actor (a seed node, sort of)
-    val master = system.actorOf(Props(new Master(workersAmount, numbers, tickerSymbol, listener)), name = "master")
+    val master = system.actorOf(Props(new Master(workersAmount, indexName, companyNames, listener)), name = "master")
 
     // Start the calculation
     master ! Calculate
 
+    // Meet the IndexService Future value requirement
+    Future.successful(1.0)
+
   }
+
 }
 
-class Master(workersAmount: Int, numbers: List[Double], stockMarketIndex: String, listener: ActorRef) extends Actor {
+class Master(workersAmount: Int, indexName: IndexName, companyNames: Seq[String], listener: ActorRef) extends Actor {
   var calculationResult: Double = 0.0
+  var listOfResults: ListBuffer[Double] = ListBuffer()
   var resultsCount: Integer = 0
-  val numbersAmount: Integer = numbers.length
+  val companiesAmount: Integer = companyNames.length
 
   val workerRouter = context.actorOf(
     Props[Worker].withRouter(RoundRobinPool(workersAmount)), name = "workerRouterPool")
 
   def receive = {
+
     case Calculate =>
-      for (i <- 0 until numbersAmount ) workerRouter ! Work(numbers(i))
+      for (i <- 0 until companiesAmount ) workerRouter ! Work(indexName, companyNames(i))
+
     case PartialResult(value) =>
       calculationResult += value
+      listOfResults += value
       resultsCount += 1
-      if (resultsCount == numbersAmount) {
+      if (resultsCount == companiesAmount) {
         // Send the result to the listener
-        listener ! StockIndexValue(calculationResult, stockMarketIndex)
+        listener ! StockIndexValue(listOfResults, indexName, companyNames)
         // Stops this actor and all its supervised children
         context.stop(self)
       }
+
   }
 
 }
 
 class Worker extends Actor {
-  def calculateStockIndexFor(number: Double): Double = {
-    var result = number * Math.E
+
+  def calculateStockIndexFor(indexName: IndexName, companyName: String): Double = {
+
+    val result : Double = indexName match {
+
+      case IndexName.AVERAGE_TRUE_RANGE => {
+
+        var res : Double = 0.0
+        val date = Date.from(LocalDate.of(2016,1,1).atStartOfDay().atZone(ZoneId.systemDefault()).toInstant)
+        val quotation: Future[Seq[Quotation]] = QuotationService.getByCompanyName(companyName)
+        res = StockIndexAlgorithms.calculateAverageValueForStockForGivenDay(quotation, date)
+        res
+
+      }
+
+      case IndexName.EASE_OF_MOVEMENT => {
+
+        var res : Double = 0.0
+        val pastFromDate = Date.from(LocalDate.of(2016,1,1).atStartOfDay().atZone(ZoneId.systemDefault()).toInstant)
+        val pastToDate = Date.from(LocalDate.of(2016,2,1).atStartOfDay().atZone(ZoneId.systemDefault()).toInstant)
+        val presentFromDate = Date.from(LocalDate.of(2016,3,1).atStartOfDay().atZone(ZoneId.systemDefault()).toInstant)
+        val presentToDate = Date.from(LocalDate.of(2016,4,1).atStartOfDay().atZone(ZoneId.systemDefault()).toInstant)
+        val quotation: Future[Seq[Quotation]] = QuotationService.getByCompanyName(companyName)
+        res = StockIndexAlgorithms.calculateEaseOfMovement(quotation, pastFromDate, pastToDate, presentFromDate, presentToDate)
+        res
+
+      }
+
+      case IndexName.MOVING_AVERAGE => {
+
+        var res = new ListBuffer[Double]()
+        val fromDate = Date.from(LocalDate.of(2016,1,1).atStartOfDay().atZone(ZoneId.systemDefault()).toInstant)
+        val toDate = Date.from(LocalDate.of(2017,1,1).atStartOfDay().atZone(ZoneId.systemDefault()).toInstant)
+        val quotation: Future[Seq[Quotation]] = QuotationService.getByCompanyName(companyName)
+        res = StockIndexAlgorithms.calculateMovingAveragesOnActorSystem(quotation, fromDate, toDate)
+        res.head
+
+      }
+
+      case _ => 0.0
+
+    }
+
     result
+
   }
 
   def receive = {
-    case Work(number: Double) =>
-      sender ! PartialResult(calculateStockIndexFor(number)) // Perform the work
+    case Work(indexName: IndexName, companyName: String) =>
+      sender ! PartialResult(calculateStockIndexFor(indexName, companyName)) // Perform the work
   }
 }
 
 class Listener extends Actor {
   def receive = {
-    case StockIndexValue(stockIndexValue, stockMarketIndex) =>
-      println("\n\tStock index approximation: \t\t%s\n\tFor stock symbol: %s".format(stockIndexValue, stockMarketIndex))
+    case StockIndexValue(listOfResults, indexName, companyNames) =>
+
+      println("\n\t-===============================-"
+        + "\tStock index approximation results"
+        + "\tFor stock index %s".format(indexName)
+        + "\t-===============================-")
+
+      for ( (name, value) <- companyNames zip listOfResults) {
+        println("\n\tFor ticker symbol (company name): %s".format(name))
+        println("\tValue of index %s: %s".format(indexName, value.toString()))
+      }
+
       context.system.shutdown()
   }
 }
